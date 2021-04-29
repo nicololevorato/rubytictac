56351f1506e8085834249399348c841ab4c42f86
nicololevorato <nicololevorato@gmail.com>
2021-04-29 11:43:45 +0200

    aggiunto logica di gioco, rimane bug in cui se inserisci stringa seleziona casella 0

--- rubytictac.rb
+++ rubytictac.rb
@@ -1,7 +1,7 @@
+LINES = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 class Game
-    @@board=Array.new(9)
-    attr_accessor:board
     def initialize
+        @board=Array.new(9)
         @player1=Player.new('X')
         loop do
             @player2=Player.new('O')
@@ -11,9 +11,10 @@ class Game
             puts "Nome già scelto!"
         end
     end
-    @@board[0]='X'
-    def print_board
-        @@board.each_with_index do |symbol, index| 
+    attr_accessor:board
+    #@board[0]='X'
+    def print_board 
+        @board.each_with_index do |symbol, index| 
             print "[#{symbol.nil? ? index : symbol}]"
             if (index+1)%3==0
                 puts "","----+-----+----"
@@ -22,6 +23,45 @@ class Game
             end
         end
     end
+    def cell_selection(player)
+        puts "#{player.name} sceglie la casella"
+        n=gets.chomp.to_i
+        if(@board[n].nil? && 0<=n && n<=8)
+            @board[n]=player.symbol
+        else 
+            puts "Casella non disponibile!"
+            cell_selection(player)
+        end
+    end
+    def has_win?(player)
+        LINES.any? do |line|
+            line.all? do |position|
+                @board[position]==player.symbol   
+            end
+        end
+    end
+    def has_free_space?
+        @board.any? do |cell|
+            cell.nil?
+        end
+    end
+    def play
+        currentPlayer=@player1
+        while (has_free_space?) do
+            print_board
+            cell_selection(currentPlayer)
+            if has_win?(currentPlayer)
+                puts "#{currentPlayer.name} complimenti hai vinto!"
+                break
+            end
+            currentPlayer=(currentPlayer==@player1) ? @player2 : @player1
+        end
+        print_board
+        if !(has_free_space?) 
+            puts "PAREGGIO!"
+        end
+    end
+
 end
 
 class Player
@@ -34,4 +74,4 @@ class Player
     attr_reader:name
 end
 
-Game.new.print_board
+Game.new.play
